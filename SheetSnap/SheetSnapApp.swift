90baa83c3683e03c64687ceb9eb2db489aa5da59
casstao <casstao@gmail.com>
2026-04-01 11:34:11 -0400

    Initial Commit

---
+++ SheetSnap/SheetSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetSnapApp.swift
+//  SheetSnap
+//
+//  Created by Felicia Hou on 3/31/26.
+//
+
+import SwiftUI
+
+@main
+struct SheetSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SheetSnapUITests/SheetSnapUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SheetSnapUITestsLaunchTests.swift
+//  SheetSnapUITests
+//
+//  Created by Felicia Hou on 3/31/26.
+//
+
+import XCTest
+
+final class SheetSnapUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
